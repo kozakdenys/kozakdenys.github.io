eb547a52ebcff35e9a3b989cfc900e846036ff42
dkozak <dkozak@dio-soft.com>
2019-09-15 03:20:29 +0300

    Add app

---
+++ precache-manifest.ef4c7443a3671329475ebe168c0ab70c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1897b9a74fe6b917e3edbfca6cf869d3",
+    "url": "/LCNPUBD/index.html"
+  },
+  {
+    "revision": "ce168699d5e880dfa1d6",
+    "url": "/LCNPUBD/static/css/2.19331af0.chunk.css"
+  },
+  {
+    "revision": "2c6b8e2c468646234c71",
+    "url": "/LCNPUBD/static/css/main.603014d8.chunk.css"
+  },
+  {
+    "revision": "ce168699d5e880dfa1d6",
+    "url": "/LCNPUBD/static/js/2.1f5ea490.chunk.js"
+  },
+  {
+    "revision": "2c6b8e2c468646234c71",
+    "url": "/LCNPUBD/static/js/main.e5eccf8c.chunk.js"
+  },
+  {
+    "revision": "42ed9ad66d8bb2b0d804",
+    "url": "/LCNPUBD/static/js/runtime~main.b1863c2c.js"
+  },
+  {
+    "revision": "1a7925ff96284abba19787786c047eb5",
+    "url": "/LCNPUBD/static/media/loader.1a7925ff.gif"
+  }
+]);
